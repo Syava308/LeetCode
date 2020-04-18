65523ff09736398dd93931e8615c62c09f4ccbf7
Syava308 <vasiliy308@gmail.com>
2020-04-19 00:03:46 +0200

    init project

---
+++ src/main/java/solution/LongestPalindromicSubstring.java
@@ -0,0 +1,19 @@
+package solution;
+
+public class LongestPalindromicSubstring {
+    public boolean Test()
+    {
+        if(1!=1)
+        {
+            return false;
+        }
+
+        return true;
+    }
+    public String solution()
+    {
+        String result = "";
+
+        return result;
+    }
+}
