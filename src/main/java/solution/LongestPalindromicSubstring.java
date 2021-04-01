240d41ba84673b73df9ffb97765468c01aa9173f
Syava308 <vasiliy308@gmail.com>
2021-04-01 12:56:41 +0200

    Add LongestPalindromicSubstring solution

--- src/main/java/solution/LongestPalindromicSubstring.java
+++ src/main/java/solution/LongestPalindromicSubstring.java
@@ -1,18 +1,37 @@
 package solution;
 
 public class LongestPalindromicSubstring {
-    public boolean Test()
-    {
-        if(1!=1)
-        {
-            return false;
-        }
 
-        return true;
-    }
-    public String solution()
+    public String solution(String input)
     {
         String result = "";
+        char[] str = input.toCharArray();
+        int pal = 0;
+        int k = 1;
+        for(int i = 0; i < str.length; i++)
+        {
+            //char c = input.charAt(i);
+            while (i+1 < str.length && str[i]==str[i+1])
+            {
+                i++;
+                pal++;
+            }
+            while ( i-pal-k >= 0 && i + k < str.length && str[i-pal-k] == str[i+k])
+            {
+                k++;
+            }
+
+            if (result.length() < 2*k+pal && (pal > 0 || k > 1))
+            {
+                result = input.substring(i-pal - k + 1, i+k);
+            }
+            else if (result.length() == 0)
+            {
+                result = String.valueOf(str[i]);
+            }
+            k=1;
+            pal=0;
+        }
 
         return result;
     }
